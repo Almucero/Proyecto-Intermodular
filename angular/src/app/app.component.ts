52279c0fbe90a126876c49e65bd3ca98a6437e77
Almucero <alvarokilor@gmail.com>
2025-11-24 13:20:37 +0100

    file-upload shared component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StrapiAuthService } from './core/services/impl/strapi-auth.service';
 import { ErrorToastComponent } from './shared/components/error-toast/error-toast.component';
+import { LanguageService } from './core/services/language.service';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,7 @@ export class AppComponent {
   title = 'GameSage';
 
   strapiAuth: StrapiAuthService = inject(StrapiAuthService);
+  private languageService = inject(LanguageService);
 
   constructor() {
     this.strapiAuth;
