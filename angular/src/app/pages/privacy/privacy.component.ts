bb3809974fead747e36cddb451ff075422d6f988
Rosario <ayigonzalezortiz@gmail.com>
2025-11-25 19:54:45 +0100

    cambios de diseño

--- angular/src/app/pages/privacy/privacy.component.ts
+++ angular/src/app/pages/privacy/privacy.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy',
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, TranslatePipe],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss',
 })
