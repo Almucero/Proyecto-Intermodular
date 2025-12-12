5beb0d8fabef68b65a5a5b6864e866871b8ad6a9
Almucero <alvarokilor@gmail.com>
2025-12-12 14:26:04 +0100

    pantalla de carga básica

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { trigger } from '@angular/animations';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err),
-);
+setTimeout(() => {
+  bootstrapApplication(AppComponent, appConfig).catch((err) =>
+    console.error(err)
+  );
+}, 3000);
